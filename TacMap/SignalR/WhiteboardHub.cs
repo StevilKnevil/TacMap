135b8ef5ced00378beaf1a3d0992ca0c896f0567
Stevil Knevil <stevilknevil@hotmail.co.uk>
2017-09-25 18:11:51 +0100

    Items now written to database (I think)

--- TacMap/SignalR/WhiteboardHub.cs
+++ TacMap/SignalR/WhiteboardHub.cs
@@ -13,48 +13,74 @@ namespace TacMap.SignalR
   {
     Dictionary<string, SqlConnection> sqlConnectionLookup = new Dictionary<string, SqlConnection>();
 
+    private bool checkTableExists(SqlConnection conn, string tableName)
+    {
+      // https://stackoverflow.com/questions/464474/check-if-a-sql-table-exists
+      bool exists = false;
+      try
+      {
+        // ANSI SQL way.  Works in PostgreSQL, MSSQL, MySQL.  
+        using (var cmd = new SqlCommand(
+          "select case when exists((select * from information_schema.tables where table_name = '" + tableName + "')) then 1 else 0 end"))
+        {
+          conn.Open();
+          cmd.Connection = conn;
+          exists = (int)cmd.ExecuteScalar() == 1;
+          conn.Close();
+        }
+      }
+      catch
+      {
+        exists = false;
+      }
+      return exists;
+    }
+
     public SqlConnection EnsureDBConnection(string groupName)
     {
+      // Ensure that groupName is correctly formatted
+      System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex("^[a-zA-Z0-9]*$");
+      if (!r.IsMatch(groupName))
+      {
+        throw new ArgumentException("Only alpha-numeirc characters allowed");
+      }
+
       if (!sqlConnectionLookup.ContainsKey(groupName))
       {
         // pass all the info so far.
+        string dbName = "drawItems";
         string dataSource = @"(localdb)\MSSQLLocalDB";
         string connStr = string.Format(@"Data Source={0};Initial Catalog={1};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
-          dataSource, groupName);
+          dataSource, dbName);
 
         // TODO: before connecting make sure the DB exists:
         // https://stackoverflow.com/questions/2232227/check-if-database-exists-before-creating
+        var newConnection = new SqlConnection(connStr);
 
-        // See if it exists (and create if not)
+        // See if the table exists for this (and create if not)
         {
-          // TODO create a new connection to master.dbo.sysdatabases
-          using (var cmd = new SqlCommand($"SELECT db_id('@dbName')"))
+          if (!checkTableExists(newConnection, groupName))
           {
-            cmd.Parameters.AddWithValue("@dbName", "groupName");
-            conn.Open();
-            if (cmd.ExecuteScalar() == DBNull.Value)
+            // Create the db
+            // https://stackoverflow.com/questions/9015142/creating-a-database-programmatically-in-sql-server
+            string createCommandStr = @"CREATE TABLE {0} (" +
+                "[Tool] TINYINT NOT NULL, " +
+                "[StartX] SMALLINT NOT NULL, " +
+                "[StartY] SMALLINT NOT NULL, " +
+                "[EndX] SMALLINT NOT NULL, " +
+                "[EndY] SMALLINT NOT NULL" +
+                ");";
+            using (var createCommand = new SqlCommand(String.Format(createCommandStr, groupName)))
             {
-              // Create the db
-              // https://stackoverflow.com/questions/9015142/creating-a-database-programmatically-in-sql-server
-              string createCommandStr = @"CREATE TABLE @dbName (
-                [Tool] TINYINT NOT NULL, 
-                [StartX] SMALLINT NOT NULL, 
-                [StartY] SMALLINT NOT NULL, 
-                [EndX] SMALLINT NOT NULL, 
-                [EndY] SMALLINT NOT NULL
-            );";
-              using (var createCommand = new SqlCommand(createCommandStr))
-              {
-                createCommand.Parameters.AddWithValue("@dbName", "groupName");
-                createCommand.ExecuteNonQuery();
-              }
-              conn.Close();
+              newConnection.Open();
+              createCommand.Connection = newConnection;
+              createCommand.ExecuteNonQuery();
+              newConnection.Close();
             }
           }
         }
 
         // TODO: Dispose of these on shutdown
-        var newConnection = new SqlConnection(connStr);
         sqlConnectionLookup[groupName] = newConnection;
       }
       return sqlConnectionLookup[groupName];
@@ -75,25 +101,27 @@ namespace TacMap.SignalR
 
     public void SendDraw(string drawObject, string sessionId, string groupName, string name)
     {
+      // Ensure that groupName is correctly formatted
+      System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex("^[a-zA-Z0-9]*$");
+      if (!r.IsMatch(groupName))
+      {
+        throw new ArgumentException("Only alpha-numeirc characters allowed");
+      }
+
       Backend.DrawItem di = Backend.DrawItem.FromJson(drawObject);
 
-      /*
-CREATE TABLE [dbo].[drawItems] (
-    [Tool] TINYINT NOT NULL, 
-    [StartX] SMALLINT NOT NULL, 
-    [StartY] SMALLINT NOT NULL, 
-    [EndX] SMALLINT NOT NULL, 
-    [EndY] SMALLINT NOT NULL
-);
-       */
       if (di.DrawState == Backend.DrawItem.DrawStates.Completed)
       {
         SqlConnection sqlCon = EnsureDBConnection(groupName);
         sqlCon.Open();
-        var insertCommand = "INSERT INTO dbo.drawItems (Tool) VALUES(@item)";
-        using (var cmd = new SqlCommand(insertCommand, sqlCon))
+        var insertCommand = "INSERT INTO {0} (Tool, StartX, StartY, EndX, EndY) VALUES(@tool, @startX, @startY, @endX, @endY)";
+        using (var cmd = new SqlCommand(String.Format(insertCommand, groupName), sqlCon))
         {
-          cmd.Parameters.AddWithValue("@item", "frist");
+          cmd.Parameters.AddWithValue("@tool", di.Tool);
+          cmd.Parameters.AddWithValue("@startX", di.StartX);
+          cmd.Parameters.AddWithValue("@startY", di.StartY);
+          cmd.Parameters.AddWithValue("@endX", di.CurrentX);
+          cmd.Parameters.AddWithValue("@endY", di.CurrentY);
           cmd.ExecuteNonQuery();
         }
         sqlCon.Close();
